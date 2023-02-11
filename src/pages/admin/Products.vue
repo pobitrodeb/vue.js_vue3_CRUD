52956dd907014d52e96a9155912b71b763c92899
Pobitrodeb <pobitrodn@gmail.com>
2023-02-11 14:14:18 +0600

    product view show

--- src/pages/admin/Products.vue
+++ src/pages/admin/Products.vue
@@ -1,13 +1,46 @@
 <template>
-    Products 
+  <table>
+    <thead>
+      <tr>
+        <th></th>
+        <th>Title</th>
+        <th>Image</th>
+        <th>Actions</th>
+      </tr>
+    </thead>
+
+    <tbody>
+      <tr v-for="product in products" :key="product.id">
+        <td>{{ product.id }}</td>
+        <td>{{ product.title }}</td>
+        <td><img :src="product.image" :alt="product.title" width="90" /></td>
+        <td>
+          <router-link
+            :to="{ name: 'ProductEdit', params: { id: product.id } }"
+            class="btn"
+            >Edit</router-link
+          >
+          <button @click="del(product.id)" class="btn btn-del">Delete</button>
+        </td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
 <script>
-    export default {
-        name: "Products", 
-    }
+import { onMounted, ref } from 'vue'; 
+export default {
+  name: "Products",
+  setup() {
+    const products = ref([]);
+    onMounted( async () => {
+        const res = await fetch('httpls://localhost:3000/products');
+        products.value = await res.json(); 
+    });
+
+    return {products}
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
